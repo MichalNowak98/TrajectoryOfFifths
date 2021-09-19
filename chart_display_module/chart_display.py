c80fcec2856b2ec7fb47b4eb479855171b49093f
michnow363 <michnow363@student.polsl.pl>
2021-09-19 16:05:12 +0200

    Refactored code Saved magic numbers to constants Function generate_graph only creates basic blank chart now, generate_music_signature_graph and generate_trajectory_of_fifths_graph uses this function and generates further data

--- chart_display_module/chart_display.py
+++ chart_display_module/chart_display.py
@@ -1,28 +1,44 @@
-from math import sin, cos
+from math import sin, cos, pi
 
 LEGEND = ["C", "G", "D", "A", "E", "B", "Gb", "Db", "Ab", "Eb", "Bb", "F"]
+# 30 degrees equals ~0.524 in radians
+ANGLE = pi / 6
+LABEL_MARGIN = 1.05
 
 
-def generate_graph(window, graph_size, number_of_lines, point_table):
-    graph_middle = graph_size / 2 + 25
-    graph = window['graph']  # type: sg.Graph
-    unit_length = graph_size / number_of_lines / 2
+def calculate_middle_and_unit_length(graph_size, margin, number_of_lines):
+    graph_middle = graph_size / 2
+    # reducing unit length by margin to make space for legend
+    unit_length = (graph_size - margin) / number_of_lines / 2
+    return graph_middle, unit_length
+
+
+def generate_graph(graph, graph_size, margin, graph_middle, unit_length, number_of_lines):
     # lines and legend
     for i in range(12):
-        # 30 degrees equals ~0.524 in radians
-        x = sin(0.524 * i) * unit_length * number_of_lines
-        y = cos(0.524 * i) * unit_length * number_of_lines
-        graph.draw_text(LEGEND[i], (graph_middle + x * 1.05, graph_middle + y * 1.05))
+        x = sin(ANGLE * i) * unit_length * number_of_lines
+        y = cos(ANGLE * i) * unit_length * number_of_lines
+        graph.draw_text(LEGEND[i], (graph_middle + x * LABEL_MARGIN, graph_middle + y * LABEL_MARGIN))
         graph.draw_line((graph_middle, graph_middle), (graph_middle + x, graph_middle + y), color='grey70')
     # circles
     for i in range(number_of_lines):
         graph.draw_circle((graph_middle, graph_middle), unit_length * (i + 1))
+
+
+def generate_music_signature_graph(graph, graph_size, margin, point_table):
+    graph_middle, unit_length = calculate_middle_and_unit_length(graph_size, margin, 1)
+    generate_graph(graph, graph_size, margin, graph_middle, unit_length, 1)
     # points
     for point in point_table:
         x = graph_middle + point[0] * unit_length
         y = graph_middle + point[1] * unit_length
         graph.draw_point((x, y), 5, color='green')
 
-
-def generate_music_signature_graph(window, graph_size, point_table):
-    generate_graph(window, graph_size, 1)
+def generate_trajectory_of_fifths_graph(graph, graph_size, margin, point_table, number_of_lines):
+    graph_middle, unit_length = calculate_middle_and_unit_length(graph_size, margin, number_of_lines)
+    generate_graph(graph, graph_size, margin, graph_middle, unit_length, number_of_lines)
+    # points
+    for point in point_table:
+        x = graph_middle + point[0] * unit_length
+        y = graph_middle + point[1] * unit_length
+        graph.draw_point((x, y), 5, color='green')
--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import PySimpleGUI as sg
-from chart_display_module.chart_display import generate_graph, generate_music_signature_graph
-import os.path
-import shutil
-import tempfile
-import io
-from PIL import Image, ImageColor, ImageDraw
+from chart_display_module.chart_display import generate_trajectory_of_fifths_graph, generate_music_signature_graph
 
 GRAPH_SIZE = 500
+MARGIN = 50
 GRAPH_MIDDLE = GRAPH_SIZE / 2
 LINES = 3
 
 if __name__ == '__main__':
     layout = [[sg.Graph(canvas_size=(GRAPH_SIZE, GRAPH_SIZE), graph_bottom_left=(0, 0),
                         graph_top_right=(GRAPH_SIZE, GRAPH_SIZE), background_color='white',
-                        enable_events=True, key='graph')],
-              [sg.Text('Change circle color to:'), sg.Button('Red'), sg.Button('Blue'), sg.Button('Move')]]
+                        enable_events=True, key='graph')]]
 
     window = sg.Window('Trajectory of fifths', layout, finalize=True)
-    generate_graph(window, GRAPH_SIZE - 50, LINES, [[0, 1], [1, 0], [0.3, 0.4]])
+    generate_trajectory_of_fifths_graph(window['graph'], GRAPH_SIZE, MARGIN, [[1, 0]], 5)
 
     while True:
         event, values = window.read()
