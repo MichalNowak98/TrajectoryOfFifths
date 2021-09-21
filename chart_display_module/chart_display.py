b685098299f12ce803bc6e596ea019d858346f03
michnow363 <michnow363@student.polsl.pl>
2021-09-21 11:55:33 +0200

    Refactored code Created PitchClass enum Changed middle of the graph to 0,0, deleted unnecessary graph_middle variable

--- chart_display_module/chart_display.py
+++ chart_display_module/chart_display.py
@@ -1,44 +1,65 @@
 from math import sin, cos, pi
+from enum import Enum
 
-LEGEND = ["C", "G", "D", "A", "E", "B", "Gb", "Db", "Ab", "Eb", "Bb", "F"]
+
+class PitchClass(Enum):
+    A = 0
+    D = 1
+    G = 2
+    C = 3
+    F = 4
+    Bb = 5
+    Eb = 6
+    Ab = 7
+    Db = 8
+    Gb = 9
+    B = 10
+    E = 11
+
+
 # 30 degrees equals ~0.524 in radians
 ANGLE = pi / 6
+ANGLE_MARGIN = pi/2
 LABEL_MARGIN = 1.05
 
 
-def calculate_middle_and_unit_length(graph_size, margin, number_of_lines):
-    graph_middle = graph_size / 2
+def calculate_unit_length(graph_size, margin, number_of_lines):
     # reducing unit length by margin to make space for legend
-    unit_length = (graph_size - margin) / number_of_lines / 2
-    return graph_middle, unit_length
+    unit_length = (graph_size - margin) / number_of_lines
+    return unit_length
 
 
-def generate_graph(graph, graph_size, margin, graph_middle, unit_length, number_of_lines):
+def count_coordinates(angle_multiplier, unit_length, value_multiplier):
+    angle = -ANGLE * angle_multiplier + ANGLE_MARGIN
+    return sin(angle) * unit_length * value_multiplier, cos(angle) * unit_length * value_multiplier
+
+
+def generate_graph(graph, graph_size, margin, unit_length, number_of_lines):
     # lines and legend
-    for i in range(12):
-        x = sin(ANGLE * i) * unit_length * number_of_lines
-        y = cos(ANGLE * i) * unit_length * number_of_lines
-        graph.draw_text(LEGEND[i], (graph_middle + x * LABEL_MARGIN, graph_middle + y * LABEL_MARGIN))
-        graph.draw_line((graph_middle, graph_middle), (graph_middle + x, graph_middle + y), color='grey70')
+    for pitch_class in PitchClass:
+        x, y = count_coordinates(pitch_class.value, unit_length, number_of_lines)
+        graph.draw_text(pitch_class.name, (x * LABEL_MARGIN, y * LABEL_MARGIN))
+        graph.draw_line((0, 0), (x, y), color='grey70')
     # circles
     for i in range(number_of_lines):
-        graph.draw_circle((graph_middle, graph_middle), unit_length * (i + 1))
+        graph.draw_circle((0, 0), unit_length * (i + 1))
 
 
 def generate_music_signature_graph(graph, graph_size, margin, point_table):
-    graph_middle, unit_length = calculate_middle_and_unit_length(graph_size, margin, 1)
-    generate_graph(graph, graph_size, margin, graph_middle, unit_length, 1)
+    unit_length = calculate_unit_length(graph_size, margin, 1)
+    generate_graph(graph, graph_size, margin, unit_length, 1)
     # points
-    for point in point_table:
-        x = graph_middle + point[0] * unit_length
-        y = graph_middle + point[1] * unit_length
+    for pitch_class in PitchClass:
+        x, y = count_coordinates(pitch_class.value, unit_length, point_table[pitch_class.value])
         graph.draw_point((x, y), 5, color='green')
+        graph.draw_line((0, 0), (x, y), color='green')
+
 
 def generate_trajectory_of_fifths_graph(graph, graph_size, margin, point_table, number_of_lines):
-    graph_middle, unit_length = calculate_middle_and_unit_length(graph_size, margin, number_of_lines)
-    generate_graph(graph, graph_size, margin, graph_middle, unit_length, number_of_lines)
+    unit_length = calculate_unit_length(graph_size, margin, number_of_lines)
+    generate_graph(graph, graph_size, margin, unit_length, number_of_lines)
     # points
     for point in point_table:
-        x = graph_middle + point[0] * unit_length
-        y = graph_middle + point[1] * unit_length
+        x = point[0] * unit_length
+        y = point[1] * unit_length
         graph.draw_point((x, y), 5, color='green')
--- main.py
+++ main.py
@@ -7,12 +7,14 @@ GRAPH_MIDDLE = GRAPH_SIZE / 2
 LINES = 3
 
 if __name__ == '__main__':
-    layout = [[sg.Graph(canvas_size=(GRAPH_SIZE, GRAPH_SIZE), graph_bottom_left=(0, 0),
+    layout = [[sg.Graph(canvas_size=(GRAPH_SIZE, GRAPH_SIZE), graph_bottom_left=(-GRAPH_SIZE, -GRAPH_SIZE),
                         graph_top_right=(GRAPH_SIZE, GRAPH_SIZE), background_color='white',
                         enable_events=True, key='graph')]]
 
     window = sg.Window('Trajectory of fifths', layout, finalize=True)
-    generate_trajectory_of_fifths_graph(window['graph'], GRAPH_SIZE, MARGIN, [[1, 0]], 5)
+    #generate_trajectory_of_fifths_graph(window['graph'], GRAPH_SIZE, MARGIN, [[1, 0]], 5)
+    generate_music_signature_graph(window['graph'], GRAPH_SIZE, MARGIN, [0.06, 0.5, 1, 0.96, 0.37, 0.06, 0.61, 0.49,
+                                                                         0.21, 0.1, 0.23, 0.1])
 
     while True:
         event, values = window.read()
