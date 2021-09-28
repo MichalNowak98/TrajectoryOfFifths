6e1e2911ae358e40f370e5cad13a336a9dda8fc9
michnow363 <michnow363@student.polsl.pl>
2021-09-28 10:52:02 +0200

    Code refactor Deleted calculate_cpms, PitchClass and ANGLE_BETWEEN_AXES from chart_display_module.calculations. They were moved to trajectory_of_fifths_module.calculations

--- chart_display_module/calculations.py
+++ chart_display_module/calculations.py
@@ -1,27 +1,6 @@
 from math import sin, cos, pi, ceil, sqrt, pow
 from enum import Enum
 
-# 30 degrees equals ~0.524 in radians
-ANGLE_BETWEEN_AXES = pi / 6
-
-
-class PitchClass(Enum):
-    A = 0
-    D = 1
-    G = 2
-    C = 3
-    F = 4
-    Bb = 5
-    Eb = 6
-    Ab = 7
-    Db = 8
-    Gb = 9
-    B = 10
-    E = 11
-
-    def angle(self):
-        return ANGLE_BETWEEN_AXES * self.value
-
 
 def calculate_line_length(starting_point, ending_point):
     a = starting_point[0] - ending_point[0]
@@ -45,13 +24,4 @@ def calculate_max_line_index(point_table):
     for point in point_table:
         ll = calculate_line_length((0, 0), (point[0], point[1]))
         max_line_index = max(max_line_index, ceil(calculate_line_length((0, 0), (point[0], point[1]))))
-        return max_line_index
-
-
-def calculate_cpms(point_table):
-    x = 0
-    y = 0
-    for pitch_class in PitchClass:
-        x += point_table[pitch_class.value] * cos(pitch_class.angle())
-        y += point_table[pitch_class.value] * sin(pitch_class.angle())
-    return x, y
+    return max_line_index
