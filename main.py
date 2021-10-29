361a4e7fda293d23116b3e3f6971c0f2b689814b
michnow363 <michnow363@student.polsl.pl>
2021-10-29 12:09:32 +0200

    Code refactor. Created show_music_signature, show_trajectory_of_fifths, show_comparison_of_trajectories and sustain_window. Added layouts file, containing layouts classes.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from chart_display_module.chart_display import generate_trajectory_of_fifths_graph, generate_music_signature_graph
-import PySimpleGUI as sg
+import PySimpleGUI
 from midi_module.midi_analyzer import get_cpms_array_whole_file, get_cpms_array_quarter_notes, get_cpms_array_from_csv
+from layouts import ComparisonGraphLayout, GraphLayout
+
 
 NUMBER_OF_CHUNKS = 32
 GRAPH_SIZE = 700
@@ -21,30 +23,52 @@ TRAJECTORY_TEST_ARRAYS = [
     [(0.32, 1), (-1.12, -1.09), (0.11, 2.1), (1, -3.1)]
 ]
 
-if __name__ == '__main__':
-    track = "I_Prelude22_4"
-    midi_cpms = get_cpms_array_quarter_notes('d:/Studia/Magisterka/Trajektorie/MIDI/{}.mid'.format(track), NUMBER_OF_CHUNKS)
-    csv_cpms = get_cpms_array_from_csv('d:/Studia/Magisterka/Trajektorie/CPMS/{}_quarter_note_from-0-to-32_Trajektoria.csv'.format(track))
 
-    layout = [
-        [
-            sg.Graph(canvas_size=(GRAPH_SIZE, GRAPH_SIZE), graph_bottom_left=(-GRAPH_SIZE, -GRAPH_SIZE),
-                        graph_top_right=(GRAPH_SIZE, GRAPH_SIZE), background_color='white',
-                        enable_events=True, key='cvs_graph'),
-            sg.Graph(canvas_size=(GRAPH_SIZE, GRAPH_SIZE),graph_bottom_left=(-GRAPH_SIZE, -GRAPH_SIZE),
-                        graph_top_right=(GRAPH_SIZE, GRAPH_SIZE), background_color='white',
-                        enable_events=True, key='midi_graph')
-        ]
-    ]
+def show_music_signature():
+    graph_layout = GraphLayout(GRAPH_SIZE)
+    window = PySimpleGUI.Window('Characteristic point of music signature', graph_layout.layout, finalize=True)
+    generate_music_signature_graph(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, SIGNATURE_TEST_ARRAYS[7])
+    sustain_window(window)
 
-    window = sg.Window('Trajectory of fifths', layout, finalize=True)
-    generate_trajectory_of_fifths_graph(window['cvs_graph'], GRAPH_SIZE, MARGIN, csv_cpms)
-    generate_trajectory_of_fifths_graph(window['midi_graph'], GRAPH_SIZE, MARGIN, midi_cpms)
-    #generate_music_signature_graph(window['midi_graph'], GRAPH_SIZE, MARGIN, SIGNATURE_TEST_ARRAYS[7])
 
+def show_trajectory_of_fifths():
+    graph_layout = GraphLayout(GRAPH_SIZE)
+    window = PySimpleGUI.Window('Trajectory of fifths', graph_layout.layout, finalize=True)
+    track = "I_Prelude3_8"
+    midi_cpms = get_cpms_array_quarter_notes('d:/Studia/Magisterka/Trajektorie/MIDI/{}.mid'.format(track),
+                                             NUMBER_OF_CHUNKS)
+    generate_trajectory_of_fifths_graph(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, midi_cpms)
+    sustain_window(window)
+
+
+def show_comparison_of_trajectories():
+    graph_layout = ComparisonGraphLayout(GRAPH_SIZE)
+    window = PySimpleGUI.Window('Comparison of trajectories of fifths', graph_layout.layout, finalize=True)
+    track = "I_Prelude3_8"
+    midi_cpms = get_cpms_array_quarter_notes('d:/Studia/Magisterka/Trajektorie/MIDI/{}.mid'.format(track),
+                                             NUMBER_OF_CHUNKS)
+    csv_cpms = get_cpms_array_from_csv(
+        'd:/Studia/Magisterka/Trajektorie/CPMS/{}_quarter_note_from-0-to-32_Trajektoria.csv'.format(track))
+    generate_trajectory_of_fifths_graph(window[graph_layout.LEFT_GRAPH_KEY], GRAPH_SIZE, MARGIN, midi_cpms)
+    generate_trajectory_of_fifths_graph(window[graph_layout.RIGHT_GRAPH_KEY], GRAPH_SIZE, MARGIN, csv_cpms)
+    sustain_window(window)
+
+
+def sustain_window(window):
     while True:
         event, values = window.read()
         print(event, values)
-        if event == sg.WIN_CLOSED:
+        if event == PySimpleGUI.WIN_CLOSED:
             break
     window.close()
+
+
+if __name__ == '__main__':
+    show_music_signature()
+
+
+
+
+
+
+
