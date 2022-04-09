7932a1d75ec33130cbfc2b77dd744b51e28ec464
michnow363 <michnow363@student.polsl.pl>
2022-04-09 16:20:36 +0200

    Added main directed axis

--- chart_display_module/chart_display.py
+++ chart_display_module/chart_display.py
@@ -1,17 +1,20 @@
-from chart_display_module.calculations import calculate_coordinates,\
-    calculate_unit_length, calculate_max_line_index
-from trajectory_of_fifths_module.calculations import PitchClass, calculate_cpms, find_main_axis_signature,\
+from chart_display_module.calculations import calculate_coordinates, \
+    calculate_unit_length, calculate_max_line_index, calculate_line_length
+from trajectory_of_fifths_module.calculations import PitchClass, calculate_cpms, find_main_axis_signature, \
     angle_between_vector_and_x_axis, find_main_axis_trajectory
 
 LABEL_MARGIN = 1.05
 CARET_LENGTH_MULTIPLIER = 0.05
 CPMS_FORMATTER = "{0:.2f}"
+LINES_WIDTH = 3
+
+FONT = ("Arial", 20)
 
 
 def generate_graph(graph, graph_size, margin, unit_length, number_of_lines):
     # lines and legend
     for pitch_class in PitchClass:
-        x, y = calculate_coordinates(pitch_class.angle(), unit_length, number_of_lines)
+        x, y = calculate_coordinates(pitch_class.angle(), unit_length, 2)
         graph.draw_text(pitch_class.label(), (x * LABEL_MARGIN, y * LABEL_MARGIN))
         graph.draw_line((0, 0), (x, y), color='grey70')
     # circles
@@ -20,32 +23,80 @@ def generate_graph(graph, graph_size, margin, unit_length, number_of_lines):
         graph.draw_text(i + 1, (unit_length * (i + 1) * LABEL_MARGIN, -50), color='grey40')
 
 
-def generate_music_signature_graph(graph, graph_size, margin, note_class_durations):
+def generate_music_signature_graph_with_directed_axis(graph, graph_size, margin, note_class_durations):
+    generate_music_signature_graph_for_note_class_durations(graph, graph_size, margin, note_class_durations)
+    main_axis_pitch_class = find_main_axis_signature(note_class_durations)
     x, y = calculate_cpms(note_class_durations)
     max_line_index = calculate_max_line_index([(x, y)])
+    unit_length = calculate_unit_length(graph_size, margin, max_line_index)
+    draw_main_directed_axis(graph, main_axis_pitch_class, unit_length, max_line_index)
+    draw_mode_axis(graph, main_axis_pitch_class, unit_length, max_line_index)
+
+
+def generate_music_signature_graph_for_note_class_durations_with_directed_axis(graph, graph_size, margin,
+                                                                               note_class_durations):
+    generate_music_signature_graph_for_note_class_durations(graph, graph_size, margin, note_class_durations)
     main_axis_pitch_class = find_main_axis_signature(note_class_durations)
+    x, y = calculate_cpms(note_class_durations)
+    max_line_index = calculate_max_line_index([(x, y)])
+    unit_length = calculate_unit_length(graph_size, margin, max_line_index)
+    draw_main_directed_axis(graph, main_axis_pitch_class, unit_length, max_line_index)
+    draw_mode_axis(graph, main_axis_pitch_class, unit_length, max_line_index)
+
+
+def generate_music_signature_graph_for_note_class_durations(graph, graph_size, margin, note_class_durations):
+    x, y = calculate_cpms(note_class_durations)
+    max_line_index = calculate_max_line_index([(x, y)])
 
     unit_length = calculate_unit_length(graph_size, margin, max_line_index)
     generate_graph(graph, graph_size, margin, unit_length, max_line_index)
 
     # CPMS point
     graph.draw_point((x * unit_length, y * unit_length), 10, color='red')
-    graph.draw_text("CPMS " + CPMS_FORMATTER.format(x) + ", " + CPMS_FORMATTER.format(y), (x * unit_length * LABEL_MARGIN, y * unit_length * LABEL_MARGIN), color='red')
+    graph.draw_text("CPMS " + CPMS_FORMATTER.format(x) + ", " + CPMS_FORMATTER.format(y),
+                    (x * unit_length + 20, y * unit_length + 40), color='red')
+
+    # vectors
+    for pitch_class in PitchClass:
+        x, y = calculate_coordinates(pitch_class.angle(), unit_length, note_class_durations[pitch_class.value])
+        if x != 0 or y != 0:
+            graph.draw_point((x, y), 7, color='blue')
+            graph.draw_line((0, 0), (x, y), color='blue', width=LINES_WIDTH)
+            draw_caret(graph, pitch_class, unit_length, max_line_index, x, y, 'blue')
+
+
+def generate_music_signature_graph_for_point(graph, graph_size, margin, point):
+    x, y = point
+    max_line_index = calculate_max_line_index([(x, y)])
+
+    unit_length = calculate_unit_length(graph_size, margin, max_line_index)
+    generate_graph(graph, graph_size, margin, unit_length, max_line_index)
+
+    # CPMS point
+    graph.draw_point((x * unit_length, y * unit_length), 10, color='red')
+    graph.draw_text("CPMS " + CPMS_FORMATTER.format(x) + ", " + CPMS_FORMATTER.format(y),
+                    (x * unit_length + 20, y * unit_length + 40), color='red')
 
     # vectors
     for pitch_class in PitchClass:
         x, y = calculate_coordinates(pitch_class.angle(), unit_length, note_class_durations[pitch_class.value])
         graph.draw_point((x, y), 7, color='blue')
-        graph.draw_line((0, 0), (x, y), color='blue', width=3)
+        graph.draw_line((0, 0), (x, y), color='blue', width=LINES_WIDTH)
 
+
+def generate_trajectory_of_fifths_graph_with_directed_axis(graph, graph_size, margin, cpms_table):
+    max_line_index = calculate_max_line_index(cpms_table)
+    unit_length = calculate_unit_length(graph_size, margin, max_line_index)
+    generate_graph(graph, graph_size, margin, unit_length, max_line_index)
+    main_axis_pitch_class = find_main_axis_trajectory(cpms_table)
     draw_main_directed_axis(graph, main_axis_pitch_class, unit_length, max_line_index)
+    draw_mode_axis(graph, main_axis_pitch_class, unit_length, max_line_index)
 
 
 def generate_trajectory_of_fifths_graph(graph, graph_size, margin, cpms_table):
     max_line_index = calculate_max_line_index(cpms_table)
     unit_length = calculate_unit_length(graph_size, margin, max_line_index)
     generate_graph(graph, graph_size, margin, unit_length, max_line_index)
-    main_axis_pitch_class = find_main_axis_trajectory(cpms_table)
 
     # points and lines between them
     for point in range(len(cpms_table)):
@@ -57,15 +108,13 @@ def generate_trajectory_of_fifths_graph(graph, graph_size, margin, cpms_table):
         if point != 0:
             graph.draw_line((previous_x, previous_y), (x, y), color='blue')
 
-    draw_main_directed_axis(graph, main_axis_pitch_class, unit_length, max_line_index)
-
 
 def draw_main_directed_axis(graph, main_axis_pitch_class, unit_length, number_of_lines):
     x1, y1 = calculate_coordinates(main_axis_pitch_class.angle(), unit_length, number_of_lines)
     x2, y2 = calculate_coordinates(PitchClass.class_for_index(main_axis_pitch_class.value + 6).angle(), unit_length,
                                    number_of_lines)
-    draw_dashed_line(graph, x1, y1, x2, y2, 30)
-    draw_caret(graph, main_axis_pitch_class, unit_length, number_of_lines)
+    draw_dashed_line(graph, x1, y1, x2, y2, 30, 'red')
+    draw_caret(graph, main_axis_pitch_class, unit_length, number_of_lines, x1, y1, 'red')
     # draw_main_axis_label(graph, PitchClass.class_for_index(main_axis_pitch_class.value - 1), unit_length,
     #                     number_of_lines)
 
@@ -75,17 +124,31 @@ def draw_main_axis_label(graph, pitch_class, unit_length, number_of_lines):
     graph.draw_text(pitch_class.name, (x * LABEL_MARGIN, y * LABEL_MARGIN), color='red')
 
 
-def draw_caret(graph, main_axis_pitch_class, unit_length, number_of_lines):
-    x, y = calculate_coordinates(main_axis_pitch_class.angle(), unit_length, number_of_lines)
-    x1, y1 = calculate_coordinates(main_axis_pitch_class.angle() + 0.05, unit_length*0.9, number_of_lines)
-    x2, y2 = calculate_coordinates(main_axis_pitch_class.angle() - 0.05, unit_length*0.9, number_of_lines)
-    graph.draw_line((x1, y1), (x, y), color='red', width=3)
-    graph.draw_line((x2, y2), (x, y), color='red', width=3)
+def draw_caret(graph, main_axis_pitch_class, unit_length, number_of_lines, x, y, color):
+    line_length = calculate_line_length([0, 0], [x, y])
+    caret_line_angle_diff = 0.025 * (unit_length * number_of_lines / line_length)
+    if number_of_lines > 1:
+        number_of_lines = number_of_lines - 1
+    x1, y1 = calculate_coordinates(
+        main_axis_pitch_class.angle() + caret_line_angle_diff, number_of_lines - 1 + line_length - 30, number_of_lines
+    )
+    x2, y2 = calculate_coordinates(
+        main_axis_pitch_class.angle() - caret_line_angle_diff, number_of_lines - 1 + line_length - 30, number_of_lines
+    )
+    graph.draw_line((x1, y1), (x, y), color=color, width=LINES_WIDTH)
+    graph.draw_line((x2, y2), (x, y), color=color, width=LINES_WIDTH)
 
 
-def draw_dashed_line(graph, x1, y1, x2, y2, number_of_points):
+def draw_dashed_line(graph, x1, y1, x2, y2, number_of_points, color):
     for i in range(0, number_of_points, 2):
         graph.draw_line((x1 / number_of_points * i, y1 / number_of_points * i),
-                        (x1 / number_of_points * (i+1), y1 / number_of_points * (i+1)), color='red', width=3)
+                        (x1 / number_of_points * (i + 1), y1 / number_of_points * (i + 1)), color=color, width=LINES_WIDTH)
         graph.draw_line((x2 / number_of_points * i, y2 / number_of_points * i),
-                        (x2 / number_of_points * (i+1), y2 / number_of_points * (i+1)), color='red', width=3)
+                        (x2 / number_of_points * (i + 1), y2 / number_of_points * (i + 1)), color=color, width=LINES_WIDTH)
+
+
+def draw_mode_axis(graph, main_axis_pitch_class, unit_length, number_of_lines):
+    mode_axis_pitch_class = PitchClass.class_for_index(main_axis_pitch_class.value - 3)
+    x, y = calculate_coordinates(mode_axis_pitch_class.angle(), unit_length, number_of_lines)
+    draw_dashed_line(graph, 0, 0, x, y, 20, 'green')
+    draw_caret(graph, mode_axis_pitch_class, unit_length, number_of_lines, x, y, 'green')
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from chart_display_module.chart_display import generate_trajectory_of_fifths_graph, generate_music_signature_graph
+from chart_display_module.chart_display import generate_trajectory_of_fifths_graph, \
+    generate_music_signature_graph_for_note_class_durations, \
+    generate_music_signature_graph_for_note_class_durations_with_directed_axis
 import PySimpleGUI
 from midi_module.midi_analyzer import get_cpms_array_whole_file, get_cpms_array_quarter_notes, get_cpms_array_from_csv
 from layouts import ComparisonGraphLayout, GraphLayout
@@ -10,43 +12,80 @@ MARGIN = 75
 GRAPH_MIDDLE = GRAPH_SIZE / 2
 SIGNATURE_TEST_ARRAYS = [
     #[A D G C F Bb Es As Ds Gs B E]
+    [0, 0, 2/7, 0, 0, 0, 0, 0, 0, 0, 2/7, 1], #0
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1], #3
     [0.06, 0.5, 1, 0.96, 0.37, 0.06, 0.61, 0.49, 0.21, 0.1, 0.23, 0.1],
     [0, 0, 0.67, 1, 0, 0, 0.33, 0, 0, 0, 0, 0],
-    [0, 0, 0.5, 1, 0, 0, 0.5, 0, 0, 0, 0, 0],
+    [0, 0, 0.5, 1, 0, 0, 0.5, 0, 0, 0, 0, 0], #6
     [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
     [1, 0.5, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1/3, 2/3, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1/3, 2/3, 0, 0, 0, 0, 0, 0, 0, 1], #9
     [0, 0, 1, 0.5, 0, 0, 0, 0, 0, 0, 0, 0.5],
     [0.5, 0, 0, 1, 0.5, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1/5, 2/5, 1, 0, 0, 4/5, 0, 0, 0]
+    [0, 0, 0, 1/5, 2/5, 1, 0, 0, 4/5, 0, 0, 0], #12
+    #[A D G C F Bb Es As Ds Gs B E]
+    [0, 0, 6/7, 0, 0, 0, 0, 0, 0, 0, 1, 5/7],
+    [1/8, 1, 1/8, 1, 0, 0, 0, 0, 0, 1/8, 0, 1/8],
+    [0, 1/10, 7/10, 0, 0, 0, 1/10, 0, 0, 1/10, 1, 0], #15
+    [0, 0, 2/7, 0, 0, 0, 0, 0, 0, 0, 2/7, 1],
 ]
 TRAJECTORY_TEST_ARRAYS = [
-    [(0.32, 1), (-1.12, -1.09), (0.11, 2.1), (1, -3.1)]
+    [(0.32, 1), (-1.12, -1.09), (0.11, 2.1), (1, -3.1)],
+    [(1.55, -0.48), (1.16, 1.42), (0.84, -0.31), (1.15, -0.50)]
 ]
-CHOSEN_INDEX = 2
+PATHS = [
+    'd:/Studia/Magisterka/Trajektorie/MIDI/',
+    'd:/Download/'
+]
+TRACKS = [
+    "I_Prelude2_4",
+    "Metallica - Nothing Else Matters"
+]
+CHOSEN_INDEX = 15
 
 
 def show_music_signature():
     graph_layout = GraphLayout(GRAPH_SIZE)
     window = PySimpleGUI.Window('Characteristic point of music signature', graph_layout.layout, finalize=True)
-    generate_music_signature_graph(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, SIGNATURE_TEST_ARRAYS[CHOSEN_INDEX])
+    generate_music_signature_graph_for_note_class_durations(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, SIGNATURE_TEST_ARRAYS[CHOSEN_INDEX])
     sustain_window(window)
 
 
-def show_trajectory_of_fifths():
+def show_music_signature_with_directed_axis():
+    graph_layout = GraphLayout(GRAPH_SIZE)
+    window = PySimpleGUI.Window('Characteristic point of music signature', graph_layout.layout, finalize=True)
+    generate_music_signature_graph_for_note_class_durations_with_directed_axis(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, SIGNATURE_TEST_ARRAYS[CHOSEN_INDEX])
+    sustain_window(window)
+
+
+def show_trajectory_of_fifths(path):
     graph_layout = GraphLayout(GRAPH_SIZE)
     window = PySimpleGUI.Window('Trajectory of fifths', graph_layout.layout, finalize=True)
-    track = "I_Prelude3_8"
-    midi_cpms = get_cpms_array_quarter_notes('d:/Studia/Magisterka/Trajektorie/MIDI/{}.mid'.format(track),
-                                             NUMBER_OF_CHUNKS)
+    midi_cpms = get_cpms_array_quarter_notes(path, NUMBER_OF_CHUNKS)
     generate_trajectory_of_fifths_graph(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, midi_cpms)
     sustain_window(window)
 
 
-def show_comparison_of_trajectories():
+def show_trajectory_of_fifths_for_array(array):
+    graph_layout = GraphLayout(GRAPH_SIZE)
+    window = PySimpleGUI.Window('Trajectory of fifths', graph_layout.layout, finalize=True)
+    generate_trajectory_of_fifths_graph(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, array)
+    sustain_window(window)
+
+
+def show_cpms_of_trajectory(path, index):
+    graph_layout = GraphLayout(GRAPH_SIZE)
+    window = PySimpleGUI.Window('Comparison of trajectories of fifths', graph_layout.layout, finalize=True)
+    midi_cpms = get_cpms_array_quarter_notes(path, NUMBER_OF_CHUNKS)
+    generate_music_signature_graph_for_note_class_durations(window[graph_layout.GRAPH_KEY], GRAPH_SIZE, MARGIN, midi_cpms[index])
+    sustain_window(window)
+
+
+def show_comparison_of_trajectories(track):
     graph_layout = ComparisonGraphLayout(GRAPH_SIZE)
     window = PySimpleGUI.Window('Comparison of trajectories of fifths', graph_layout.layout, finalize=True)
-    track = "I_Prelude3_8"
     midi_cpms = get_cpms_array_quarter_notes('d:/Studia/Magisterka/Trajektorie/MIDI/{}.mid'.format(track),
                                              NUMBER_OF_CHUNKS)
     csv_cpms = get_cpms_array_from_csv(
@@ -66,12 +105,9 @@ def sustain_window(window):
 
 
 if __name__ == '__main__':
+    show_trajectory_of_fifths_for_array(TRAJECTORY_TEST_ARRAYS[1])
     #show_music_signature()
-    show_trajectory_of_fifths()
-
-
-
-
-
-
-
+    #show_music_signature_with_directed_axis()
+    #show_trajectory_of_fifths(PATHS[1] + TRACKS[1] + ".mid")
+    #show_cpms_of_trajectory(PATHS[1] + TRACKS[1] + ".mid", 1)
+    #show_comparison_of_trajectories()
